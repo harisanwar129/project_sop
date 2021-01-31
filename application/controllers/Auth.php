dbcb68a989b12114ebc6376d92d360d746b7b44e
harisanwar129 <harisanwar129@gmail.com>
2021-01-31 22:30:12 +0700

    5.login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,4 +8,29 @@ class Auth extends CI_Controller
     {
         $this->load->view('login');
     }
+    public function process()
+    {
+        $post = $this->input->post(null, TRUE);
+        if (isset($post['login'])) {
+            $this->load->model('user_m');
+            $query = $this->user_m->login($post);
+            if ($query->num_rows() > 0) {
+                $row = $query->row();
+                $params = array(
+                    'userid' => $row->user_id,
+                    'level' => $row->level
+                );
+                $this->session->set_userdata($params);
+                echo "<script>
+                        alert('Selamat,Login Berhasil');
+                        window.location='" . site_url('dashboard') . "';
+                </script>";
+            } else {
+                echo "<script>
+                    alert('Login Gagal,username/password salah');
+                    window.location='" . site_url('auth/login') . "';
+                     </script>";
+            }
+        }
+    }
 }
