ad3eb3cf8109982a66152bc6c20516809a6f75e0
harisanwar129 <harisanwar129@gmail.com>
2021-02-14 13:29:13 +0700

    11.validasi dan tambah pengguna

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,16 +4,48 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User extends CI_Controller
 {
 
-    public function index()
+    function __construct()
     {
+        parent::__construct();
         check_not_login();
         $this->load->model('user_m');
+    }
+    public function index()
+    {
         $data['row'] = $this->user_m->get();
         $this->template->load('template', 'user/user_data', $data);
     }
 
     public function add()
     {
-        $this->template->load('template', 'user/user_form_add');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('fullname', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Pengguna', 'required|min_length[5]|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Kata Sandi', 'required|min_length[5]');
+        $this->form_validation->set_rules(
+            'passconf',
+            'Konfirmasi Kata Sandi',
+            'required|matches[password]',
+            array('matches' => '%s tidak sesuai dengan password')
+        );
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+        $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '{field} isi sudah terdaftar, mohon diganti');
+        $this->form_validation->set_error_delimiters('<span class="help-block">', '</span>');
+        if ($this->form_validation->run() == FALSE) {
+            $this->template->load('template', 'user/user_form_add');
+        } else {
+
+            $post = $this->input->post(null, TRUE);
+            $this->user_m->add($post);
+            if ($this->db->affected_rows() > 0) {
+                echo "<script>alert('Data Berhasil Disimpan')</script>";
+            }
+            echo "<script>window.location='" . site_url('user') . "';</script>";
+
+            // echo "proses simpan data pengguna";
+        }
     }
 }
--- application/views/user/user_form_add.php
+++ application/views/user/user_form_add.php
@@ -1,1 +1,73 @@
-Tambah data
+<section class="content-header">
+    <h1>Users
+        <small>Pengguna</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> </a></li>
+        <li class="active">Users</li>
+    </ol>
+</section>
+
+<section class="content">
+
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Tambah Pengguna</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('user') ?>" class="btn btn-warning btn-flat">
+                    <i class="fa fa-undo"></i> Kembali
+                </a>
+            </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="" method="post">
+                        <div class="form-group  <?= form_error('fullname') ? 'has-error' : null ?>">
+                            <label for="nama">Nama *</label>
+                            <input type="text" name="fullname" value="<?= set_value('fullname') ?>" class="form-control" id="nama" placeholder="Isi Nama">
+                            <?= form_error('fullname') ?>
+                        </div>
+                        <div class="form-group <?= form_error('username') ? 'has-error' : null ?>">
+                            <label for="nama_pengguna">Nama Pengguna *</label>
+                            <input type="text" name="username" value="<?= set_value('username') ?>" class="form-control" id="nama_pengguna" placeholder="Isi Nama Pengguna">
+                            <?= form_error('username') ?>
+                        </div>
+                        <div class="form-group <?= form_error('password') ? 'has-error' : null ?>">
+                            <label for="sandi">Kata Sandi Pengguna *</label>
+                            <input type="password" name="password" value="<?= set_value('password') ?>" class="form-control" id="sandi" placeholder="Isi Kata Sandi">
+                            <?= form_error('password') ?>
+                        </div>
+                        <div class="form-group <?= form_error('passconf') ? 'has-error' : null ?>">
+                            <label for="konfirmasi_sandi">Konfirmasi Kata Sandi *</label>
+                            <input type="password" name="passconf" value="<?= set_value('passconf') ?>" class="form-control" id="konfirmasi_sandi" placeholder="Isi Konfirmasi Kata Sandi">
+                            <?= form_error('passconf') ?>
+                        </div>
+                        <div class="form-group <?= form_error('address') ? 'has-error' : null ?>">
+                            <label for="alamat">Alamat *</label>
+                            <textarea name="address" value="<?= set_value('address') ?>" class="form-control" id="alamat" placeholder="Isi Alamat"></textarea>
+                            <?= form_error('address') ?>
+                        </div>
+                        <div class="form-group <?= form_error('level') ? 'has-error' : null ?>">
+                            <label for="level">Level *</label>
+                            <select name="level" id="level" class="form-control">
+                                <option value="">--Pilih--</option>
+                                <option value="1" value=<?= set_value('level') == 1 ? "selected" : null ?>>Admin</option>
+                                <option value="2" value=<?= set_value('level') == 2 ? "selected" : null ?>>Kasir</option>
+                            </select>
+                            <?= form_error('level') ?>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-flat">
+                                <i class="fa fa-paper-plane"></i> Simpan
+                            </button>
+                            <button type="reset" class="btn btn-primary">Ulangi</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
